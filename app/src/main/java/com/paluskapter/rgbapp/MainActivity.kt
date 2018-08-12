3b9f58a9244abe008892023c7e85669141a93b49
paluskapter <paluskapter@gmail.com>
2018-08-12 22:05:29 +0200

    Added color picker

--- app/src/main/java/com/paluskapter/rgbapp/MainActivity.kt
+++ app/src/main/java/com/paluskapter/rgbapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.paluskapter.rgbapp
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
@@ -8,6 +9,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.doAsync
 import java.net.URL
+import android.widget.LinearLayout
+import com.skydoves.colorpickerview.ColorListener
+import org.jetbrains.anko.info
+
 
 class MainActivity : AppCompatActivity(), AnkoLogger {
 
@@ -16,16 +21,19 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
             R.id.navigation_home -> {
                 colorLayout.visibility = View.VISIBLE
                 modeLayout.visibility = View.INVISIBLE
+                settingsLayout.visibility = View.INVISIBLE
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
                 colorLayout.visibility = View.INVISIBLE
                 modeLayout.visibility = View.VISIBLE
+                settingsLayout.visibility = View.INVISIBLE
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
                 colorLayout.visibility = View.INVISIBLE
                 modeLayout.visibility = View.INVISIBLE
+                settingsLayout.visibility = View.VISIBLE
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -48,6 +56,13 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
             doAsync {
                 resultBox.text = URL("http://192.168.0.8:5000/rainbow").readText()
             }
+        }
+
+        colorPickerView.setColorListener { color ->
+            doAsync {
+                resultBoxPicker.text = URL("http://192.168.0.8:5000/instant_color/${Color.red(color)}/${Color.green(color)}/${Color.blue(color)}").readText()
+            }
+            colorDisplay.setBackgroundColor(color)
         }
     }
 }
