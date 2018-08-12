aef2531953f16ed2aa0585de604e2b088b501c58
paluskapter <paluskapter@gmail.com>
2018-08-12 15:43:30 +0200

    MVP API calls

--- app/src/main/java/com/paluskapter/rgbapp/MainActivity.kt
+++ app/src/main/java/com/paluskapter/rgbapp/MainActivity.kt
@@ -3,22 +3,29 @@ package com.paluskapter.rgbapp
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.doAsync
+import java.net.URL
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AnkoLogger {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+                colorLayout.visibility = View.VISIBLE
+                modeLayout.visibility = View.INVISIBLE
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+                colorLayout.visibility = View.INVISIBLE
+                modeLayout.visibility = View.VISIBLE
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+                colorLayout.visibility = View.INVISIBLE
+                modeLayout.visibility = View.INVISIBLE
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -30,5 +37,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        colorButton.setOnClickListener {
+            doAsync {
+                resultBox.text = URL("http://192.168.0.8:5000/instant_color/${red.text}/${green.text}/${blue.text}").readText()
+            }
+        }
+
+        rainbowButton.setOnClickListener {
+            doAsync {
+                resultBox.text = URL("http://192.168.0.8:5000/rainbow").readText()
+            }
+        }
     }
 }
